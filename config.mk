d46200a162e57fb0995f7fa02173f547ed6f41f4
Alexandr Savca <alexandr.savca89@gmail.com>
2023-06-06 15:25:10 +0300

    config.mk: bump version to 5.42.1, indent fix

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 # project metadata
-NAME = pkgmk
-VERSION = 5.42
-DIST = ${NAME}-${VERSION}
+NAME        = pkgmk
+VERSION     = 5.42.1
+DIST        = ${NAME}-${VERSION}
 
 # paths
-PREFIX = /usr
-MANPREFIX = ${PREFIX}/share/man
+PREFIX      = /usr
+MANPREFIX   = ${PREFIX}/share/man
 BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
