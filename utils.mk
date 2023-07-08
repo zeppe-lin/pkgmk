9cf54b182cfad533d7a6d0b1a4550a526b55a32f
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 07:45:43 +0300

    utils.mk: restyle, add longlines target

--- utils.mk
+++ utils.mk
@@ -1,10 +1,15 @@
-GREP_DEFS = --exclude-dir=.git --exclude-dir=.github -R .
+GREPOPT = --exclude-dir=.git --exclude-dir=.github -R .
+FINDOPT = -not \( -path "./.git*" -or -path ".*~" \)
+MAXLINE = 80
 
 urlcodes:
 	@echo "=======> Check URLs for response code"
-	@grep -Eiho "https?://[^\"\\'> ]+" ${GREP_DEFS}  \
-		| xargs -P10 -I{} curl -o /dev/null      \
-		 -sw "[%{http_code}] %{url}\n" '{}'      \
+	@grep -Eiho "https?://[^\"\\'> ]+" ${GREPOPT} \
+		| grep -v "http://www.gnu.org/software/somelib/index.html" \
+		| grep -Ev "https?://\*/\*"           \
+		| grep -v "http://xyz.org/"           \
+		| xargs -P10 -I{} curl -o /dev/null   \
+		 -sw "[%{http_code}] %{url}\n" '{}'   \
 		| sort -u
 
 podchecker:
@@ -13,7 +18,11 @@ podchecker:
 
 shellcheck:
 	@echo "=======> Check shell scripts for syntax errors"
-	@grep -m1 -l '^#\s*!/bin/sh' ${GREP_DEFS}        \
-		| xargs -L10 shellcheck -s sh
+	@grep -m1 -l '^#\s*!/bin/sh' ${GREPOPT} | xargs -L10 shellcheck -s sh
 
-.PHONY: urlcodes podchecker shellcheck
+longlines:
+	@echo "=======> Check for long lines (> ${MAXLINE})"
+	@find . -type f ${FINDOPT} -exec awk -v ML=${MAXLINE} \
+		'length > ML { print FILENAME ":" FNR " " $$0 }'  {} \;
+
+.PHONY: urlcodes podchecker shellcheck longlines
