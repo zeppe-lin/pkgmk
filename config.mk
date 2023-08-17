b663c66c266fd5092ace22cc313c69070cdff041
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-17 18:12:42 +0300

    config.mk: add HOMEPAGE and BUGTRACKER variables

--- config.mk
+++ config.mk
@@ -1,6 +1,8 @@
 # project metadata
 NAME        = pkgmk
 VERSION     = 5.43
+HOMEPAGE    = https://github.com/zeppe-lin/pkgmk/
+BUGTRACKER  = https://github.com/zeppe-lin/pkgmk/issues/
 DIST        = ${NAME}-${VERSION}
 
 # paths
