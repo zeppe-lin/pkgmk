81764fc040bae55a93ea52096e4de6819f2923a4
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-20 21:49:29 +0300

    remove junk, clean up

--- config.mk
+++ config.mk
@@ -1,8 +1,6 @@
 # project metadata
 NAME        = pkgmk
 VERSION     = 5.43
-HOMEPAGE    = https://github.com/zeppe-lin/pkgmk/
-BUGTRACKER  = https://github.com/zeppe-lin/pkgmk/issues/
 DIST        = ${NAME}-${VERSION}
 
 # paths
